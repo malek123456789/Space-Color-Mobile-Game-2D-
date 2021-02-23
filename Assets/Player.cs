719ce6b3a3ffae4dbd5beba12048d77601b74e39
malek123456789 <56312699+malek123456789@users.noreply.github.com>
2021-02-23 22:15:39 +0100

    Add files via upload

---
+++ Assets/Player.cs
@@ -0,0 +1,75 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+public class Player : MonoBehaviour
+{
+    
+    public float jumpForce = 10f;
+
+    public Rigidbody2D rigidBody;
+    public SpriteRenderer spriteRenderer;
+
+    public string currentColor;
+
+    public Sprite rocketBlue;
+    public Sprite rocketYellow;
+    public Sprite rocketGreen;
+    public Sprite rocketRed;
+
+    private void Start()
+    {
+        SetRandomColor();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Jump") || Input.GetMouseButtonDown(0))
+        {
+            rigidBody.velocity = Vector2.up * jumpForce;
+        }
+
+    }
+
+    private void OnTriggerEnter2D (Collider2D col)
+    {
+        if (col.tag == "ColorChanger")
+        {
+            SetRandomColor();
+            Destroy(col.gameObject);
+                return;
+        }
+
+
+        if (col.tag != currentColor)
+        {
+            Debug.Log("GAME OVER");
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        }
+    }
+
+    void SetRandomColor()
+    {
+        int index = Random.Range(0, 4);
+            
+        switch (index)
+        {   
+            case 0:
+                currentColor = "Cyan";
+                spriteRenderer.sprite = rocketBlue;
+                break;
+            case 1:
+                currentColor = "Yellow";
+                spriteRenderer.sprite = rocketYellow;
+                break;
+            case 2:
+                currentColor = "Magenta";
+                spriteRenderer.sprite = rocketGreen;
+                break;
+            case 3:
+                currentColor = "Pink";
+                spriteRenderer.sprite = rocketRed;
+                break;
+        }
+    }
+}
